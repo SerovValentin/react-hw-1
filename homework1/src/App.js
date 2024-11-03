a030fb70545225ddcb2fd26cb0a5e349f2438189
valentinserov <serov.valent@yandex.ru>
2024-11-03 17:59:17 +0200

    rewrite app.js without jsx

--- homework1/src/App.js
+++ homework1/src/App.js
@@ -1,27 +1,59 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { createElement } from "react";
+
+// function App() {
+//   let date = new Date(); //декларативный
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//         {date.getFullYear()}
+//       </header>
+//     </div>
+//   ); //декларативный
+// }
 
 function App() {
-  let date = new Date(); //декларативный
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {date.getFullYear()}
-      </header>
-    </div>
-  ); //декларативный
+  let date = new Date();
+  return createElement(
+    "div",
+    { className: "App" },
+    createElement(
+      "header",
+      { className: "App-header" },
+      createElement("img", { src: logo, className: "App-logo", alt: "logo" }),
+      createElement(
+        "p",
+        null,
+        "Edit ",
+        createElement("code", null, "src/App.js"),
+        " and save to reload."
+      ),
+      createElement(
+        "a",
+        {
+          className: "App-link",
+          href: "https://reactjs.org",
+          target: "_blank",
+          rel: "noopener noreferrer",
+        },
+        "Learn React"
+      ),
+      date.getFullYear()
+    )
+  );
 }
 
 export default App;
